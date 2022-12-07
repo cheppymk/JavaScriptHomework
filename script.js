d37f37df5ef8fe025c81a139b1f3d35729f6da47
cheppymk <119892722+cheppymk@users.noreply.github.com>
2022-12-07 23:10:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const phonePrice = 119.95;
-const phoneTax = 0.05;
-const phonePostTaxPrice = phonePrice*phoneTax;
-
-let phoneNumber = prompt("how many phone want:");
-
-let result = phoneNumbers * phonePostTaxPrice;
-console.log("The total price for your order ", result);
